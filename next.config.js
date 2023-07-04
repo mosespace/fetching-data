8367388c0c2f63dc94cca1fc2fe82c5425fa8ef9
mosespace <kisakyemoses50@gmail.com>
2023-07-04 14:29:37 +0300

    configured images in yarn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     images: {
-      domains: ["placeimg.com", "picsum.photos", "cdn.pixabay.com", "fakestoreapi.com"],
+      domains: ["placeimg.com", "picsum.photos", "cdn.pixabay.com", "fakestoreapi.com", "m.media-amazon.com"],
     },
 };
   
