1e21d1b10d8bfd0b3f8dddb3a43e565cec642b3a
mosespace <kisakyemoses50@gmail.com>
2023-07-05 09:32:52 +0300

    feat! added prices in a rray

---
+++ prices.js
@@ -0,0 +1,50 @@
+export const month =[
+    {
+        id: 1,
+        title: 'Free',
+        decription: '49',
+        itemsArray: ['Cras sodales lobortis erat', 'Vitae pellentesque diam', 'Consequat eted tempus']},
+    {
+        id: 2,
+        title: 'Small Bussiness',
+        decription: '99',
+        itemsArray: ['Allquet diam gravida', 'Phasellus eu condimentum', 'Metus non venenatis turpis']},
+    {
+        id: 3,
+        title: 'Professional',
+        decription: '219',
+        itemsArray: ['Donec ominnulla malesuada', 'Sed wnenatis vel, blandit vel', 'Duis utrricles scelerisque']},
+    {
+        id: 4,
+        title: 'Enterprise',
+        decription: '419',
+        itemsArray: ['Justo Id  molestie', 'Nullan sodales justo fringilla', 'Donec molesstie nequeurna']  
+    }
+
+]
+
+export const year =[
+    {
+        id: 1,
+        title: 'Free',
+        decription: '149',
+        itemsArray: ['Your service will get the following packages', 'Your service will get the following packages', 'Your service will get the following packages']},
+    {
+        id: 2,
+        title: 'Small Bussiness',
+        decription: '299',
+        itemsArray: ['Your service will get the following packages', 'Your service will get the following packages', 'Your service will get the following packages']},
+    {
+        id: 3,
+        title: 'Professional',
+        decription: '3219',
+        itemsArray: ['Your service will get the following packages', 'Your service will get the following packages', 'Your service will get the following packages']},
+    {
+        id: 4,
+        title: 'Enterprise',
+        decription: '4419',
+        itemsArray: ['Your service will get the following packages', 'Your service will get the following packages', 'Your service will get the following packages']  
+    }
+
+]
+
